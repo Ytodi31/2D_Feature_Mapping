f1fa69b6eb49bd7432cf9f721a6ab610f4b25b5f
YTodi31 <yashaarth@todi89.com>
2020-04-25 16:37:24 +0000

    Added ring buffer for memory management and function calls for keypoint detector

--- src/MidTermProject_Camera_Student.cpp
+++ src/MidTermProject_Camera_Student.cpp
@@ -63,7 +63,10 @@ int main(int argc, const char *argv[])
         DataFrame frame;
         frame.cameraImg = imgGray;
         dataBuffer.push_back(frame);
-
+      
+      	if (dataBuffer.size()> dataBufferSize)
+          dataBuffer.erase(dataBuffer.begin());
+		cout << "Size: " << dataBuffer.size() << endl;
         //// EOF STUDENT ASSIGNMENT
         cout << "#1 : LOAD IMAGE INTO BUFFER done" << endl;
 
@@ -71,7 +74,9 @@ int main(int argc, const char *argv[])
 
         // extract 2D keypoints from current image
         vector<cv::KeyPoint> keypoints; // create empty feature list for current image
-        string detectorType = "SHITOMASI";
+        string detectorType;
+      	std::cout << "Enter the keypoint detector you wish to use: " << std::endl;
+      	std::cin >> detectorType;
 
         //// STUDENT ASSIGNMENT
         //// TASK MP.2 -> add the following keypoint detectors in file matching2D.cpp and enable string-based selection based on detectorType
@@ -81,9 +86,13 @@ int main(int argc, const char *argv[])
         {
             detKeypointsShiTomasi(keypoints, imgGray, false);
         }
-        else
+        if (detectorType.compare("HARRIS") == 0)
         {
-            //...
+             detKeypointsHarris(keypoints, imgGray, false);
+        }
+      	else
+        {
+        	detKeypointsModern(keypoints, imgGray, detectorType, false);
         }
         //// EOF STUDENT ASSIGNMENT
 
